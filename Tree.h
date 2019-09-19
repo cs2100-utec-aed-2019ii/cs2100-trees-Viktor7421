b95b1cea1357dd3306722aa70c4c491b180ee660
Unknown <victor.ascuna@utec.edu.pe>
2019-09-19 16:45:22 -0500

    feat(tree)

---
+++ Tree.h
@@ -0,0 +1,18 @@
+//
+// Created by VICTOR on 19/09/2019.
+//
+
+#ifndef UNTITLED23_TREE_H
+#define UNTITLED23_TREE_H
+
+#include "Node.h"
+
+template <typename T>
+class Tree {
+public:
+    Node<T>* root;
+
+    Tree():root(nullptr){}
+};
+
+#endif //UNTITLED23_TREE_H
